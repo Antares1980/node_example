69ac0a63f2932341a35f5109552a0dae6e5290fd
Antares1980 <ja.pv@icloud.com>
2023-02-06 13:22:48 +0100

    Creating a variable for the current date

--- src/messages.js
+++ src/messages.js
@@ -1,5 +1,5 @@
 
 exports.getGenericMessage = () => {
-  const not_used = 32;
-    return 'Hello world. It\'s ' +  new Date().getHours() + ' h ' + new Date().getMinutes() + ' m';
+  const currentDate = new Date().getHours() + ' h ' + new Date().getMinutes() + ' m';
+    return 'Hello world. It\'s ' +  currentDate;
   };
