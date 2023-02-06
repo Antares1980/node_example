bfceda9cbbdcaf21077225c6ac9ce9c0e0def326
Antares1980 <ja.pv@icloud.com>
2023-02-06 15:25:46 +0100

    Adding duplicated code to test

--- src/messages.js
+++ src/messages.js
@@ -3,3 +3,7 @@ exports.getGenericMessage = () => {
   const currentDate = new Date().getHours() + ' h ' + new Date().getMinutes() + ' m';
     return 'Hello world. It\'s ' +  currentDate;
   };
+exports.getGenericMessages = () => {
+  const currentDate = new Date().getHours() + ' h ' + new Date().getMinutes() + ' m';
+    return 'Hello world. It\'s ' +  currentDate;
+  };
