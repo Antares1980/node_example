707a82753d9e5dd2b794d602b53e26a91bb882bd
Antares1980 <ja.pv@icloud.com>
2023-02-06 15:43:25 +0100

    Adding duplication patterns file

--- src/messages.js
+++ src/messages.js
@@ -10,6 +10,7 @@ exports.getGenericMessages = () => {
   const currentDate = new Date().getHours() + ' h ' + new Date().getMinutes() + ' m';
   const a = new Date().getHours();
   const b = new Date().getMinutes();
+  
   console.log(a+b);
     return 'Hello world. It\'s ' +  currentDate;
   };
